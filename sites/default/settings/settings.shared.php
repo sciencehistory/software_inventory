3460893381e1676bd1431bb768d407c75cd8e6a0
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-09-08 09:16:05 -0400

    switching to the correct new subdomain

--- sites/default/settings/settings.shared.php
+++ sites/default/settings/settings.shared.php
@@ -712,7 +712,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 $settings['trusted_host_patterns'] = [
   '^localhost$',
   '^127.0.0.1$',
-  '^software-inventory\.chemheritage\.org$',
+  '^software-inventory\.sciencehistory\.org$',
   'example\.local$',
   'staging\.example\.com$',
   'example\.com$',
